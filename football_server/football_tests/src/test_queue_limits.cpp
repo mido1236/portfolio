4a4d1ebcd844437e104902e800cc182352f7f20a
mido1236 <mido1236@gmail.com>
2026-01-26 22:24:35 -0800

    More progress

--- football_server/football_tests/src/test_queue_limits.cpp
+++ football_server/football_tests/src/test_queue_limits.cpp
@@ -4,12 +4,17 @@
 #include <gtest/gtest.h>
 
 TEST(InputWindow, RespectsQueueLimits) {
-  const uint32_t capacity = 5;
-  const uint32_t r = 2;
+  constexpr uint32_t capacity = 5;
+  constexpr uint32_t r = 2;
   InBoundQueue q({.capacity = capacity, .reserved_for_disconnect = r});
 
   for (int i = 0; i < capacity - r; i++)
     EXPECT_EQ(q.push({}), true);
   for (int i = 0; i < 20; i++)
     EXPECT_EQ(q.push({}), false);
+
+  for (int i = 0; i < r; i++)
+    EXPECT_EQ(q.push({.type = MsgType::Disconnect}), true);
+  for (int i = 0; i < 20; i++)
+    EXPECT_EQ(q.push({.type = MsgType::Disconnect}), false);
 }
